3d6bfa0a9297fb260a37b9db2624d4407080dc44
artempaskov <pashkovchik@gmail.com>
2022-08-27 16:52:13 +0300

    Postgres add

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"gRPC_Users/internal/repository"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hi")
-	fmt.Println("gRPC_Users")
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initialising configs: %s", err.Error())
+	}
+	if err := godotenv.Load("../.env"); err != nil {
+		log.Fatalf("error loading env variables: #{err.Error()}")
+	}
+
+	_, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: #{err.Error()}")
+	}
+
+}
+
+func initConfig() error {
+	viper.AddConfigPath("../configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
